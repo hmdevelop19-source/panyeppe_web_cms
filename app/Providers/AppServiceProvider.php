ba4030f77ab88a615e659afff9d74749f6337f6d
@hmufid_ <hmdevelop19@gmail.com>
2026-05-03 12:49:08 +0700

    deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind('path.public', function() {
-            return base_path().'/../public_html';
+            return base_path().'/../cms.panyeppen.com';
         });
     }
 
