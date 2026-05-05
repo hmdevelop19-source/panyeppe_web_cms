1195d43c692987f0ff501217c690d9dbbea69300
@hmufid_ <hmdevelop19@gmail.com>
2026-05-05 10:53:13 +0700

    config test

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test-config', function() {
+    return [
+        'app_url_env' => env('APP_URL'),
+        'public_path_env' => env('PUBLIC_PATH'),
+        'public_path_resolved' => public_path(),
+        'index_exists' => file_exists(public_path('index.html')),
+    ];
+});
+
 Route::fallback(function () {
     // Jalankan React SPA jika file index.html ada di folder publik
     if (file_exists(public_path('index.html'))) {
