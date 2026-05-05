eb3c96b8ee0a203da1408bc35fafc40df95d2c0c
@hmufid_ <hmdevelop19@gmail.com>
2026-05-05 11:00:50 +0700

    prbaikan index.html

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@ Route::get('/test-config', function() {
     ];
 });
 
+Route::get('/', function() {
+    $path = public_path('index.html');
+    if (file_exists($path)) {
+        return file_get_contents($path);
+    }
+    return "File index.html tidak ditemukan di: " . $path;
+});
+
 Route::fallback(function () {
     // Jalankan React SPA jika file index.html ada di folder publik
     if (file_exists(public_path('index.html'))) {
