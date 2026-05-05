926edf2fe3635889eb2f88353172547a8706b265
@hmufid_ <hmdevelop19@gmail.com>
2026-05-05 10:27:46 +0700

    deploy ulang

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test-config', function() {
-    return [
-        'app_url_config' => config('app.url'),
-        'app_url_env' => env('APP_URL'),
-        'public_path_env' => env('PUBLIC_PATH'),
-        'public_path_resolved' => public_path(),
-    ];
-});
-
 Route::fallback(function () {
-    // Hanya dijalankan jika file index.html benar-benar ada (untuk mode single-domain)
+    // Jalankan React SPA jika file index.html ada di folder publik
     if (file_exists(public_path('index.html'))) {
         return file_get_contents(public_path('index.html'));
     }
